7efb0847306953cf6cda962b9696013e44d4808d
Vitaliy <vital040686@mail.ru>
2019-09-15 19:41:45 +0300

    penkoff was added, mb+dt. Mob needs being repaired

---
+++ Пенькоф/Архив/script.js
@@ -0,0 +1,8 @@
+function update() {
+	var updateSite = document.getElementById("js-overlay_update");
+	updateSite.classList.add("main_overlay-show"); 
+}
+function closePopup() {
+	var closePopup = document.getElementById("js-overlay_update");
+	closePopup.classList.remove("main_overlay-show"); 
+}
