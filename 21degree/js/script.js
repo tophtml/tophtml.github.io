b62190135b316fe17629662efe84e1605b02a43d
Vitaliy <vital040686@mail.ru>
2019-10-29 21:58:32 +0200

    slider on js

---
+++ 21degree/js/script.js
@@ -0,0 +1,31 @@
+let left = 0;
+let partnerArrowRight = document.querySelector('.partners-arr_right');
+let partnerArrowLeft = document.querySelector('.partners-arr_left');
+
+function moveSliderLeft() {
+	let sliderBox = document.querySelector('.partners-carousel');
+
+	partnerArrowRight.src ='img/icons/left_arrow.svg';
+	left = left - 116;
+	if (left<-580) {
+		left = -580;
+	}
+	if (left<-464) {
+		partnerArrowLeft.src ='img/icons/left_arrow_inactive.svg';
+	}
+	sliderBox.style.left = left + 'px';
+
+}
+function moveSliderRight() {
+	let sliderBox = document.querySelector('.partners-carousel');
+	
+	partnerArrowLeft.src ='img/icons/left_arrow.svg';
+	left = left + 116;
+	if (left>0) {
+		left = 0;
+	}
+	sliderBox.style.left = left + 'px';
+	if (left == 0) {
+		partnerArrowRight.src ='img/icons/left_arrow_inactive.svg';
+	}
+}
